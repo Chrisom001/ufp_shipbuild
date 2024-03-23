c53ccc0d420dc76378afd8dcaa0349dfa08f031a
Chrisom001 <cwilkinson1988@gmail.com>
2024-03-23 20:20:10 +0000

    Updating Navbar to latest code based on documentation

--- scripts/header.php
+++ scripts/header.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 session_start();
-$navbar = "";
+/*$navbar = "";
 $navbar .= '<nav class='navbar navbar-expand-lg bg-body-tertiary'>';
 $navbar .= '<div class='container-fluid'>';
 $navbar .= '<a class='navbar-brand' href='#">UFP Ship Build Tool</a>";
@@ -36,5 +36,21 @@ if(!isset($_SESSION) || empty($_SESSION)){
     $navbar .= "<a href='logout.php'>Logout</a>";
 }
 $navbar .= "</div>";
-$navbar .= "</nav>";
+$navbar .= "</nav>";*/
 ?>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link" href="#">Pricing</a>
+                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+            </div>
+        </div>
+    </div>
+</nav>

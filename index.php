73657bcb58710b7717923ffc8609b16952ab6971
Chrisom001 <cwilkinson1988@gmail.com>
2024-03-23 20:38:28 +0000

    Adding detail to homepage

--- index.php
+++ index.php
@@ -16,11 +16,9 @@ include "scripts/header.php";
 <div class="container">
     <!-- Content here -->
     <h1>Welcome to the new site</h1>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lorem enim, consectetur eu tristique ut, vestibulum eu ipsum. Vestibulum vitae bibendum purus. Fusce id risus erat. Nulla id pellentesque mauris. Sed at leo tempor, gravida arcu ullamcorper, volutpat erat. Quisque ut sem feugiat, lacinia elit vel, blandit enim. Morbi facilisis varius velit eu porta. Integer at diam justo. Aenean eget tempus nulla, nec semper neque. Aenean ac erat lectus. Praesent quam elit, efficitur non volutpat ut, dignissim at turpis. Suspendisse cursus accumsan risus sit amet cursus. Curabitur at erat mattis, varius ante vitae, feugiat orci. Vestibulum finibus placerat ligula id pulvinar. Aenean in dolor sit amet mauris gravida molestie ut ac turpis.
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lorem enim, consectetur eu tristique ut, vestibulum eu ipsum. Vestibulum vitae bibendum purus. Fusce id risus erat. Nulla id pellentesque mauris. Sed at leo tempor, gravida arcu ullamcorper, volutpat erat. Quisque ut sem feugiat, lacinia elit vel, blandit enim. Morbi facilisis varius velit eu porta. Integer at diam justo. Aenean eget tempus nulla, nec semper neque. Aenean ac erat lectus. Praesent quam elit, efficitur non volutpat ut, dignissim at turpis. Suspendisse cursus accumsan risus sit amet cursus. Curabitur at erat mattis, varius ante vitae, feugiat orci. Vestibulum finibus placerat ligula id pulvinar. Aenean in dolor sit amet mauris gravida molestie ut ac turpis.</p>
+    <p>Nam scelerisque erat vestibulum nunc imperdiet convallis. Aliquam erat volutpat. Pellentesque interdum libero metus, id feugiat turpis dapibus at. Sed sollicitudin venenatis massa, vel scelerisque enim facilisis nec. Sed turpis nisl, iaculis ac magna quis, mattis cursus dolor. Aliquam sapien ex, scelerisque tempus elit sit amet, tempus porttitor massa. Nullam sodales euismod bibendum. Sed gravida erat ac enim finibus finibus. Curabitur nec fringilla urna. Maecenas laoreet dui eget ultricies tincidunt. Vivamus vel semper purus. Aenean elementum auctor quam, in dictum metus consequat ut. Etiam ullamcorper quam gravida enim scelerisque scelerisque.</p>
 
-        Nam scelerisque erat vestibulum nunc imperdiet convallis. Aliquam erat volutpat. Pellentesque interdum libero metus, id feugiat turpis dapibus at. Sed sollicitudin venenatis massa, vel scelerisque enim facilisis nec. Sed turpis nisl, iaculis ac magna quis, mattis cursus dolor. Aliquam sapien ex, scelerisque tempus elit sit amet, tempus porttitor massa. Nullam sodales euismod bibendum. Sed gravida erat ac enim finibus finibus. Curabitur nec fringilla urna. Maecenas laoreet dui eget ultricies tincidunt. Vivamus vel semper purus. Aenean elementum auctor quam, in dictum metus consequat ut. Etiam ullamcorper quam gravida enim scelerisque scelerisque.
-    </p>
     <div class="container">
         <!-- Content here -->
         <h2>Latest Builds</h2>
@@ -36,6 +34,32 @@ include "scripts/header.php";
                 </ul>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
+
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Test Ship 1</h5>
+                    <p class="card-text">This will contain a brief summary</p>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Faction</li>
+                        <li class="list-group-item">Ship Type</li>
+                        <li class="list-group-item">Tier Level</li>
+                    </ul>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Test Ship 1</h5>
+                        <p class="card-text">This will contain a brief summary</p>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Faction</li>
+                            <li class="list-group-item">Ship Type</li>
+                            <li class="list-group-item">Tier Level</li>
+                        </ul>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
         </div>
     </div>
 </div>

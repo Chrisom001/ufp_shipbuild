3a386d217c8a663fbb508ff4eb9fba05515c28ee
Chrisom001 <cwilkinson1988@gmail.com>
2024-03-23 20:36:27 +0000

    Adding detail to homepage

--- index.php
+++ index.php
@@ -6,18 +6,43 @@ include "scripts/header.php";
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>UFP Ship Builder Tool (Dev)</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!--- Stylesheet include -->
+    <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
 <?php echo $navbar; ?>
-<div class="alert alert-primary" role="alert">
-    A simple primary alert—check it out!
+<div class="container">
+    <!-- Content here -->
+    <h1>Welcome to the new site</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lorem enim, consectetur eu tristique ut, vestibulum eu ipsum. Vestibulum vitae bibendum purus. Fusce id risus erat. Nulla id pellentesque mauris. Sed at leo tempor, gravida arcu ullamcorper, volutpat erat. Quisque ut sem feugiat, lacinia elit vel, blandit enim. Morbi facilisis varius velit eu porta. Integer at diam justo. Aenean eget tempus nulla, nec semper neque. Aenean ac erat lectus. Praesent quam elit, efficitur non volutpat ut, dignissim at turpis. Suspendisse cursus accumsan risus sit amet cursus. Curabitur at erat mattis, varius ante vitae, feugiat orci. Vestibulum finibus placerat ligula id pulvinar. Aenean in dolor sit amet mauris gravida molestie ut ac turpis.
+
+        Nam scelerisque erat vestibulum nunc imperdiet convallis. Aliquam erat volutpat. Pellentesque interdum libero metus, id feugiat turpis dapibus at. Sed sollicitudin venenatis massa, vel scelerisque enim facilisis nec. Sed turpis nisl, iaculis ac magna quis, mattis cursus dolor. Aliquam sapien ex, scelerisque tempus elit sit amet, tempus porttitor massa. Nullam sodales euismod bibendum. Sed gravida erat ac enim finibus finibus. Curabitur nec fringilla urna. Maecenas laoreet dui eget ultricies tincidunt. Vivamus vel semper purus. Aenean elementum auctor quam, in dictum metus consequat ut. Etiam ullamcorper quam gravida enim scelerisque scelerisque.
+    </p>
+    <div class="container">
+        <!-- Content here -->
+        <h2>Latest Builds</h2>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Test Ship 1</h5>
+                <p class="card-text">This will contain a brief summary</p>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Faction</li>
+                    <li class="list-group-item">Ship Type</li>
+                    <li class="list-group-item">Tier Level</li>
+                </ul>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
 </div>
-<h1>Hello, world!</h1>
+
+
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-<!--- Stylesheet include -->
-<link href="css/style.css" rel="stylesheet">
 </body>
 <?php
 include "scripts/footer.php";

307ebc8bef6bebaa2f0220d66094d96e166bcf80
Chris Wilkinson <cwilkinson1988@gmail.com>
2024-05-05 18:53:13 +0100

    Work on Build view

--- buildview.php
+++ buildview.php
@@ -15,6 +15,18 @@ include "scripts/header.php";
 <?php echo $navbar; ?>
 <div class="container">
 <!-- Content Here -->
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec non suscipit velit, vel vehicula justo. Etiam fermentum porttitor ligula eu euismod. Aliquam erat volutpat. Suspendisse convallis tincidunt dui, eu hendrerit sem ultrices a. Suspendisse faucibus augue nec mauris rutrum, vel pellentesque nunc tempor. Vestibulum vitae scelerisque nunc. Etiam tristique, magna eget lobortis lobortis, tellus nisl dignissim ex, vel gravida odio nunc at ligula. Nullam hendrerit mauris ac tellus pretium, quis ultricies neque faucibus. Mauris a orci sagittis, porttitor mi sed, ullamcorper mauris. Aliquam dapibus nunc mi, ac faucibus magna eleifend nec.
+
+        Quisque malesuada mauris quis lacus finibus accumsan. Integer at leo lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget sapien dictum arcu accumsan porta quis vel massa. Aenean auctor nibh risus, id semper odio hendrerit vitae. Mauris et tempor lorem, eget venenatis tortor. Donec nec leo velit. Maecenas at feugiat dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+    <div class="row">
+        <div class="col">
+            <p>Column 1</p>
+        </div>
+        <div class="col">
+            <p>Column 2</p>
+        </div>
+    </div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>

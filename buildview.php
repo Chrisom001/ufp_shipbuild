b05398212af1c14dc8585035a029682e3499c1fe
Chris Wilkinson <cwilkinson1988@gmail.com>
2024-05-05 19:40:31 +0100

    Work on Build view - Consoles

--- buildview.php
+++ buildview.php
@@ -133,6 +133,42 @@ include "scripts/header.php";
             </div>
         </div>
     </div>
+    <p>Seating for the ship will go here</p>
+    <p>Slot1:Type Here</p>
+    <div class="row">
+        <div class="col">Officer Power 1</div>
+        <div class="col">Officer Power 1</div>
+        <div class="col">Officer Power 1</div>
+        <div class="col">Officer Power 1</div>
+    </div>
+    <p>Slot2:Type Here</p>
+    <div class="row">
+        <div class="col">Officer Power 1</div>
+        <div class="col">Officer Power 1</div>
+        <div class="col">Officer Power 1</div>
+        <div class="col">Officer Power 1</div>
+    </div>
+    <p>Slot3:Type Here</p>
+    <div class="row">
+        <div class="col">Officer Power 1</div>
+        <div class="col">Officer Power 1</div>
+        <div class="col">Officer Power 1</div>
+        <div class="col">Officer Power 1</div>
+    </div>
+    <p>Slot4:Type Here</p>
+    <div class="row">
+        <div class="col">Officer Power 1</div>
+        <div class="col">Officer Power 1</div>
+        <div class="col">Officer Power 1</div>
+        <div class="col">Officer Power 1</div>
+    </div>
+    <p>Slot5:Type Here</p>
+    <div class="row">
+        <div class="col">Officer Power 1</div>
+        <div class="col">Officer Power 1</div>
+        <div class="col">Officer Power 1</div>
+        <div class="col">Officer Power 1</div>
+    </div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>

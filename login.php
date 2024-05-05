41a3a8831a46ffe1c9ccbec6819bf6a31d2e35a0
Chris Wilkinson <cwilkinson1988@gmail.com>
2024-05-05 19:59:32 +0100

    Added Login page

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+include "scripts/header.php";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>UFP Ship Builder Tool (Dev)</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!--- Stylesheet include -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+<?php echo $navbar; ?>
+<div class="container">
+    <form>
+        <div class="form-floating mb-3">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+        </div>
+        <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+<footer class="footer">
+    <?php include "scripts/footer.php"; ?>
+</footer>
+</html>

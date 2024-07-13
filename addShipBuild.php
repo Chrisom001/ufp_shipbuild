21c6d9c05d39b44918a8c75decbb98c5cd2d7eac
Chris Wilkinson <cwilkinson1988@gmail.com>
2024-07-13 13:10:57 +0100

    Working on add ship build page

--- addShipBuild.php
+++ addShipBuild.php
@@ -16,14 +16,72 @@ include "scripts/header.php";
 <div class="container">
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id pulvinar erat. Vivamus fringilla odio in elementum porta. Fusce fermentum augue vitae mauris fermentum interdum. Nulla fringilla sagittis volutpat. Aenean fermentum nisl non posuere suscipit. Quisque pulvinar interdum felis, ut fringilla nisi placerat ac. Quisque at nisi massa. Pellentesque imperdiet vel eros vel condimentum.</p>
     <p>Curabitur fermentum ut massa sed molestie. Proin et laoreet urna. Mauris id justo molestie, consequat augue et, consequat risus. Sed vel vehicula turpis. Nulla consectetur, ipsum et condimentum aliquet, nisi ante ornare orci, ac rhoncus felis arcu a magna. Donec feugiat, risus in aliquet varius, turpis ligula bibendum odio, quis feugiat lectus leo facilisis felis. Pellentesque eu auctor nulla, eget dignissim urna. Curabitur ac nisi ut diam pharetra mattis vitae ornare enim. Nullam luctus efficitur ipsum, sed imperdiet felis dapibus ut. Pellentesque tristique lacinia mollis. Ut nec magna libero. Fusce dictum elementum fringilla. Praesent porttitor accumsan dapibus. Curabitur viverra imperdiet semper. Mauris quis sodales purus. Praesent id bibendum arcu.</p>
-
+    <p>Select the options for your build below:</p>
     <form>
         <select class="form-select" aria-label="Default select example">
-            <option selected>Open this select menu</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
+            <option selected>Select the type of ship</option>
+            <option value="1">Ship A</option>
+            <option value="2">Ship B</option>
+            <option value="3">Ship C</option>
         </select>
+        <p>Enter the relevant weapons</p>
+        <label>Fore Weapon 1: </label>
+        <select class="form-select" aria-label="Default select example">
+            <option selected>Select the class of weapon</option>
+            <option value="1">Phaser</option>
+            <option value="2">Maser</option>
+            <option value="3">Quaser</option>
+        </select>
+        <select class="form-select" aria-label="Default select example">
+            <option selected>Select the type of Weapon</option>
+            <option value="1">Beam Array</option>
+            <option value="2">Cannon</option>
+            <option value="3">Torpedo</option>
+        </select>
+        <select class="form-select" aria-label="Default select example">
+            <option selected>Select the level of weapon</option>
+            <option value="1">Mk I</option>
+            <option value="2">Mk II</option>
+            <option value="3">Mk III</option>
+        </select>
+        <select class="form-select" aria-label="Default select example">
+            <option selected>Select the first modifier</option>
+            <option value="1">ACC</option>
+            <option value="2">ACC2</option>
+            <option value="3">ACC3</option>
+        </select>
+        <select class="form-select" aria-label="Default select example">
+            <option selected>Select the second modifier</option>
+            <option value="1">ACC</option>
+            <option value="2">ACC2</option>
+            <option value="3">ACC3</option>
+        </select>
+        <select class="form-select" aria-label="Default select example">
+            <option selected>Select the third modifier</option>
+            <option value="1">ACC</option>
+            <option value="2">ACC2</option>
+            <option value="3">ACC3</option>
+        </select>
+        <select class="form-select" aria-label="Default select example">
+            <option selected>Select the fourth modifier</option>
+            <option value="1">ACC</option>
+            <option value="2">ACC2</option>
+            <option value="3">ACC3</option>
+        </select>
+        <label>Fore Weapon 2: </label>
+
+        <label>Fore Weapon 3: </label>
+
+        <label>Fore Weapon 4: </label>
+
+        <label>Rear Weapon 1: </label>
+
+        <label>Rear Weapon 1: </label>
+
+        <label>Rear Weapon 1: </label>
+
+        <label>Rear Weapon 1: </label>
+
     </form>
 
 </div>

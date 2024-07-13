3771ebc4808d39447f7de82d44ac231721de172d
Chris Wilkinson <cwilkinson1988@gmail.com>
2024-07-13 14:53:53 +0100

    Adding more for shipbuid

--- model/api_shipType.php
+++ model/api_shipType.php
@@ -4,43 +4,16 @@ $db = new dbObj();
 $pdo =  $db->getConnstring();
 
 function getShipWeaponSlots($shipTypeID){
-    if($shipTypeID == 1){
-        $myObject = new stdClass();
-        $myObject->foreSlots = 1;
-        $myObject->rearSlots = 1;
+    global $pdo;
+    $readShipWeaponSlots = "SELECT frontSlot, rearSlot FROM ships WHERE id = $shipTypeID";
 
-        $myJson = json_encode($myObject);
+    $readShipWeaponSlotsQuery = $pdo -> query($readShipWeaponSlots);
+    $weaponSlots = $readShipWeaponSlotsQuery -> fetchAll(PDO::FETCH_OBJ);
 
-        return $myJson;
-    } elseif ($shipTypeID == 2){
-        $myObject = new stdClass();
-        $myObject->foreSlots = 2;
-        $myObject->rearSlots = 2;
-
-        $myJson = json_encode($myObject);
-
-        return $myJson;
-    } elseif ($shipTypeID == 3){
-        $myObject = new stdClass();
-        $myObject->foreSlots = 3;
-        $myObject->rearSlots = 3;
-
-        $myJson = json_encode($myObject);
-
-        return $myJson;
-    } elseif ($shipTypeID == 4){
-        $myObject = new stdClass();
-        $myObject->foreSlots = 4;
-        $myObject->rearSlots = 4;
-
-        $myJson = json_encode($myObject);
-
-        return $myJson;
-    } else {
-    return "Error"; }
+    return json_encode($weaponSlots);
 }
 
 function getShipEquipmentSlots($shipTypeID){
-    
+
 }
 ?>

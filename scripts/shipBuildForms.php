6097383c9cead1741714aee3623158d9493f98b3
Chris Wilkinson <cwilkinson1988@gmail.com>
2024-07-13 20:38:01 +0100

    Adding more for shipbuid

--- scripts/shipBuildForms.php
+++ scripts/shipBuildForms.php
@@ -12,7 +12,7 @@
             $form .= "<div class='row'>";
             $form .= "<div class='col'>";
             $form .= "<select class=form-select' aria-label='shipTierSelector' id='shipTierSelector' name='shipTierSelector'>";
-            $form .= "<option value=''>Select an option</option>";
+            $form .= "<option selected>Select the Tier of ship</option>";
             for($j=0;$j<sizeof($usableTierList);$j++){
                 $shipTierID = $usableTierList[$j]->id;
                 $shipTierText = $usableTierList[$j]->shipTier;

3705d1e476a620fbe6d4cbc4d2622829351fd1b8
Chrisom001 <cwilkinson1988@gmail.com>
2025-03-01 18:13:48 +0000

    Updated Front Page for variable form

--- buildview.php
+++ buildview.php
@@ -61,6 +61,7 @@ if(!isset($_GET["id"])){
                     $shipItems = json_decode(readItemCombination($id));
                     for($i=0; $i < $fore; $i++){
                         foreach($shipItems as $shipItem){
+                            var_dump($shipItem);
                             if($shipItem->isFrontWeapon == 1){
                                 echo "<div class='col'>";
                                 echo "<p>Fore Weapon " . $i+1 . "</p>";

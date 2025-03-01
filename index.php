15f97ad92abb53ad1209f7bce3071c0f93f48915
Chrisom001 <cwilkinson1988@gmail.com>
2025-03-01 15:33:48 +0000

    Updated Front Page for variable form

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 include "scripts/header.php";
 include "model/api_shipBuild.php"
 $getAllShipsJson = json_decode(getLatestShipBuilds());

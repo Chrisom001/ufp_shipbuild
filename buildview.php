c00841bf102cacc81311c27c626a89b3ca382a72
Chrisom001 <cwilkinson1988@gmail.com>
2025-03-01 18:23:15 +0000

    Updated Front Page for variable form

--- buildview.php
+++ buildview.php
@@ -66,7 +66,7 @@ if(!isset($_GET["id"])){
                             if($shipItem->isFrontWeapon == 1){
                                 echo "<div class='col'>";
                                 echo "<p>Fore Weapon " . $i+1 . "</p>";
-                                echo "<p>".json_decode(getDamageTypeByID($shipItem->damageTypeID)) . " " . json_decode(getEquipmentNameById($shipItem->equipmentTypeID)) . json_decode(getItemTierById($shipItem->itemTierID)). " "."</p>";
+                                echo "<p>".json_decode(getDamageTypeByID($shipItem->damageTypeID)) . " " . json_decode(getEquipmentNameById($shipItem->equipmentTypeID)) . " MK " . json_decode(getItemTierById($shipItem->itemTierID)). " "."</p>";
                                 echo "</div>";
                             }
                         }

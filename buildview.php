e9acba05bd80e0c98bc11e9ab5ea65c5f853b6ff
Chrisom001 <cwilkinson1988@gmail.com>
2025-03-01 16:09:26 +0000

    Updated Front Page for variable form

--- buildview.php
+++ buildview.php
@@ -6,6 +6,7 @@ if(!isset($_GET["id"])){
     header("Location: index.php");
 } else {
     $id = $_GET["id"];
+    echo json_decode(checkIfBuildExists($id));
     if(!json_decode(checkIfBuildExists($id))){
         header("Location: index.php");
     }

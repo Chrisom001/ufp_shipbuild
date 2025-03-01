0a4b3b63ed9673bef6f3b84a015b0fbd7c1efeee
Chrisom001 <cwilkinson1988@gmail.com>
2025-03-01 14:38:12 +0000

    Adding ship name to ship builder after selection

--- scripts/shipBuildForms.php
+++ scripts/shipBuildForms.php
@@ -60,6 +60,7 @@
         $form .= '<div class="col">';
         $getAllEquipment = json_decode(getAllEquipmentTypes());
         $form .= '<select class="form-select" aria-label="Default select example">';
+        $form .= '<option selected>Weapon Type</option>';
         for($i=0;$i<sizeof($getAllEquipment);$i++){
             if($getAllEquipment[$i]->isWeapon == 1){
                 $form .= "<option value='".$getAllEquipment[$i]->id."'>".$getAllEquipment[$i]->equipmentName."</option>";

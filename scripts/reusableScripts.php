fb58c3dd34f845f0196ddcd8dff4601a57047c62
Chrisom001 <cwilkinson1988@gmail.com>
2025-03-02 13:40:13 +0000

    Added into view builds item to pull Science, ENgineering and Tactial consoles from DB

--- scripts/reusableScripts.php
+++ scripts/reusableScripts.php
@@ -5,7 +5,6 @@ include "model/api_damageType.php";
 include "model/api_itemTiers.php";
 include "model/api_itemCombination.php";
 include "model/api_bridgeOfficers.php";
-include "model/api_shipBuild.php";
 include "model/api_users.php";
 include "model/api_shipTiers.php";
 

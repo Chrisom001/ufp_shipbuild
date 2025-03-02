969e9defa2cd4adb55a98100a7abf966d6d5e8a3
Chrisom001 <cwilkinson1988@gmail.com>
2025-03-02 00:53:11 +0000

    Updated Front Page for variable form

--- scripts/reusableScripts.php
+++ scripts/reusableScripts.php
@@ -14,24 +14,21 @@ function viewShipItems($type, $shipID){
     }
 
     $shipItems = json_decode(readItemCombination($shipID, $check));
-    //$shipItems = json_decode(readItemCombination(1, "isConsole"));
     $WeaponsArray = array();
-    //var_dump($shipItems);
-//    foreach($shipItems as $shipItem){
-//        if($shipItem->$check == 1){
-//            var_dump($shipItem);
-//            if($itemType == "console"){
-//                if($shipItem->equipmentType == $type){
-//                    echo $shipItem->equipmentType . " vs " . $type;
-//                    $consoleData = json_decode(getEquipmentNameById($shipItem->equipmentTypeID)) . " MK" . json_decode(getItemTierById($shipItem->itemTierID)) . $shipItem->equipmentType;
-//                    $WeaponsArray[] = $consoleData;
-//                }
-//            } elseif($itemType == "weapon"){
-//                $weaponData = json_decode(getDamageTypeByID($shipItem->damageTypeID)) . " " . json_decode(getEquipmentNameById($shipItem->equipmentTypeID)) . " MK" . json_decode(getItemTierById($shipItem->itemTierID));
-//                $WeaponsArray[] = $weaponData;
-//            }
-//        }
-//    }
+    foreach($shipItems as $shipItem){
+        if($shipItem->$check == 1){
+            if($itemType == "console"){
+                if($shipItem->equipmentType == $type){
+                    echo $shipItem->equipmentType . " vs " . $type;
+                    $consoleData = json_decode(getEquipmentNameById($shipItem->equipmentTypeID)) . " MK" . json_decode(getItemTierById($shipItem->itemTierID)) . $shipItem->equipmentType;
+                    $WeaponsArray[] = $consoleData;
+                }
+            } elseif($itemType == "weapon"){
+                $weaponData = json_decode(getDamageTypeByID($shipItem->damageTypeID)) . " " . json_decode(getEquipmentNameById($shipItem->equipmentTypeID)) . " MK" . json_decode(getItemTierById($shipItem->itemTierID));
+                $WeaponsArray[] = $weaponData;
+            }
+        }
+    }
     return $WeaponsArray;
 }
 ?>

46d34ecf2365a58c12206ab077158d144629888f
Chrisom001 <cwilkinson1988@gmail.com>
2025-03-02 15:37:57 +0000

    Updated menu Added image on ship build card on homepage Added ability to pull bridge officer slots

--- scripts/getrarity.php
+++ scripts/getrarity.php
@@ -1,5 +1,5 @@
 <?php
-include "../model/api_rarity.php";
+//include "../model/api_rarity.php";
 if (isset($_POST['value'])) {
 //    echo "Rarity: " . $_POST['value'];
     $value = $_POST['value'];

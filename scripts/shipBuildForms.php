2462fdb59be3787fb8db640aaa1f8bfe54d222b3
Chrisom001 <cwilkinson1988@gmail.com>
2025-03-02 14:23:12 +0000

    Updated menu Added image on ship build card on homepage Added ability to pull bridge officer slots

--- scripts/shipBuildForms.php
+++ scripts/shipBuildForms.php
@@ -53,10 +53,9 @@
         }
 
         $slotNumber = json_decode(getShipWeaponSlots($shipID, $weaponSlot));
-
+        $form = "";
         for($s = 0; $s < $slotNumber; $s++){
             echo "loop " . $s;
-            $form = "";
             $form .= '<label>'.$weaponLocation .'Weapon'. ($s + 1) . ': </label>';
             $form .= '<div class="row">';
             $form .= '<div class="col">';

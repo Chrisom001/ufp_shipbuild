abc31ec205f4ba834f3dba7134ea1528a504fc9f
Chrisom001 <cwilkinson1988@gmail.com>
2025-03-02 00:32:17 +0000

    Updated Front Page for variable form

--- scripts/reusableScripts.php
+++ scripts/reusableScripts.php
@@ -1,21 +1,25 @@
 <?php
 function viewShipItems($type, $shipID){
+    $itemType = "";
     if($type == "foreweapons"){
         $check = "isFrontWeapon";
+        $itemType = "weapon";
     } else if($type == "rearweapons"){
         $check = "isRearWeapon";
+        $itemType = "weapon";
     } elseif($type == "science" || $type == "engineering" || $type == "tactical"){
         $check = "isConsole";
+        $itemType = "console";
     }
 
     $shipItems = json_decode(readItemCombination($shipID));
     $WeaponsArray = array();
     foreach($shipItems as $shipItem){
         if($shipItem->$check == 1){
-            if($shipItem->equipmentType == $type){
+            if($itemType == "console"){
                 echo "Console" . $type;
                 $WeaponsArray[] = "Console 1: " . $type;
-            } else {
+            } elseif($itemType == "weapon"){
                 $weaponData = json_decode(getDamageTypeByID($shipItem->damageTypeID)) . " " . json_decode(getEquipmentNameById($shipItem->equipmentTypeID)) . " MK" . json_decode(getItemTierById($shipItem->itemTierID));
                 $WeaponsArray[] = $weaponData;
             }

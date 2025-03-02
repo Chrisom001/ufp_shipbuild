dd73c9ea23e56274c7a3aebad92da8f8bafbd1d7
Chrisom001 <cwilkinson1988@gmail.com>
2025-03-02 15:31:51 +0000

    Updated menu Added image on ship build card on homepage Added ability to pull bridge officer slots

--- scripts/getrarity.php
+++ scripts/getrarity.php
@@ -1,27 +1,27 @@
 <?php
 if (isset($_POST['value'])) {
     echo "Rarity: " . $_POST['value'];
-    $value = $_POST['value'];
-    $numOfModifiers = json_decode(getNumberOfModifiersByID($value));
-    var_dump($numOfModifiers);
-    if ($numOfModifiers > 0) {
-        for($i = 0; $i < $numOfModifiers; $i++) {
-            $form .= '<div class="col">';
-            $form .= '<select class="form-select" aria-label="Default select example">';
-            $form .= '<option selected>Mod 1</option>';
-            $form .= '<option value="1">ACC</option>';
-            $form .= '<option value="2">ACC2</option>';
-            $form .= '<option value="3">ACC3</option>';
-            $form .= '</select>';
-            $form .= '</div>';
-        }
-    } else {
-        $form .= '<div class="col">';
-        $form .= '<select class="form-select" aria-label="Default select example">';
-        $form .= '<option selected>No modifiers available</option>';
-        $form .= '</select>';
-        $form .= '</div>';
-    }
+//    $value = $_POST['value'];
+//    $numOfModifiers = json_decode(getNumberOfModifiersByID($value));
+//    var_dump($numOfModifiers);
+//    if ($numOfModifiers > 0) {
+//        for($i = 0; $i < $numOfModifiers; $i++) {
+//            $form .= '<div class="col">';
+//            $form .= '<select class="form-select" aria-label="Default select example">';
+//            $form .= '<option selected>Mod 1</option>';
+//            $form .= '<option value="1">ACC</option>';
+//            $form .= '<option value="2">ACC2</option>';
+//            $form .= '<option value="3">ACC3</option>';
+//            $form .= '</select>';
+//            $form .= '</div>';
+//        }
+//    } else {
+//        $form .= '<div class="col">';
+//        $form .= '<select class="form-select" aria-label="Default select example">';
+//        $form .= '<option selected>No modifiers available</option>';
+//        $form .= '</select>';
+//        $form .= '</div>';
+//    }
 } else {
     var_dump($_POST['value']);
     echo "Failure!!!";

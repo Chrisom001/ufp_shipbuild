e1561f6c70985da5635535c3f67476ca16d1509a
Chrisom001 <cwilkinson1988@gmail.com>
2025-03-02 16:42:16 +0000

    Updated menu Added image on ship build card on homepage Added ability to pull bridge officer slots

--- scripts/reusableScripts.php
+++ scripts/reusableScripts.php
@@ -130,7 +130,6 @@ function shipBuildFormHomepage(){
 }
 
 function shipCardBuilder($getShipsData){
-    var_dump($getShipsData);
     $shipBuildForm = "";
     $shipBuildForm .= "<div class='col'>";
     $shipBuildForm .= "<div class='card' style='width: 18rem;'>";

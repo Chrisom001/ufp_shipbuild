4294c620716584e32255e6ab3e974966b1f3572b
Chrisom001 <cwilkinson1988@gmail.com>
2025-03-02 00:29:20 +0000

    Updated Front Page for variable form

--- model/api_itemCombination.php
+++ model/api_itemCombination.php
@@ -30,7 +30,6 @@ function addItemCombination($shipBuild, $equipmentType, $itemType, $rarityType, 
 function readItemCombination($shipbuildID){
     global $pdo;
     $readShipBuildByID = "SELECT * FROM itemCombination INNER JOIN equipmentType ON itemCombination.equipmentTypeID = equipmentType.id INNER JOIN equipmentTypes ON equipmentType.equipmentTypeID = equipmentTypes.id WHERE shipBuildID = $shipbuildID";
-    echo $readShipBuildByID;
     $readShipBuildQuery = $pdo -> query($readShipBuildByID);
     $readShipBuilds = $readShipBuildQuery -> fetchAll(PDO::FETCH_OBJ);
 

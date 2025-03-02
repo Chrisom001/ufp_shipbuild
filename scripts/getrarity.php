e07b33f99ccfa08c38a61c5a2d3c1548d7d46ef2
Chrisom001 <cwilkinson1988@gmail.com>
2025-03-02 15:33:25 +0000

    Updated menu Added image on ship build card on homepage Added ability to pull bridge officer slots

--- scripts/getrarity.php
+++ scripts/getrarity.php
@@ -1,8 +1,9 @@
 <?php
 if (isset($_POST['value'])) {
-    echo "Rarity: " . $_POST['value'];
-//    $value = $_POST['value'];
-//    $numOfModifiers = json_decode(getNumberOfModifiersByID($value));
+//    echo "Rarity: " . $_POST['value'];
+    $value = $_POST['value'];
+    $numOfModifiers = json_decode(getNumberOfModifiersByID($value));
+    echo "Number of modifiers : ".$numOfModifiers;
 //    var_dump($numOfModifiers);
 //    if ($numOfModifiers > 0) {
 //        for($i = 0; $i < $numOfModifiers; $i++) {

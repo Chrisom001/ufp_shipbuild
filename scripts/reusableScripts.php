525dfb0bd36ecb7788cfe577c82b5065ba5ac1c2
Chrisom001 <cwilkinson1988@gmail.com>
2025-03-02 02:32:11 +0000

    Added into view builds item to pull Science, ENgineering and Tactial consoles from DB

--- scripts/reusableScripts.php
+++ scripts/reusableScripts.php
@@ -51,7 +51,7 @@ function weaponConsoleRepeater($shipID, $slotType){
     $slotArray = getNumberOfSlots($shipID);
     $sizeOfArray = count($itemArray);
     $count = 0;
-    echo getNumSlotsFromArray($slotArray, $slotType);
+
     for($i=0; $i < getNumSlotsFromArray($slotArray, $slotType); $i++) {
         $result .= "<div class='col'>";
         if ($count < $sizeOfArray) {

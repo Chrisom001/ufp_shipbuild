68efb73b9500b07c755eb987d3325fb41dc54bc4
Chrisom001 <cwilkinson1988@gmail.com>
2025-03-08 15:52:06 +0000

    Updated menu Added image on ship build card on homepage Added ability to pull bridge officer slots

--- scripts/shipBuildForms.php
+++ scripts/shipBuildForms.php
@@ -198,8 +198,7 @@ include "model/api_shipType.php";
         $form .= "<div class='tab-content' id='myTabContent'>";
         $form .= "<div class='tab-pane fade show active' id='weapon' role='tabpanel' aria-labelledby='weapon-tab'>".addShipWeps($shipID)."</div>";
         $form .= "<div class='tab-pane fade' id='consoles' role='tabpanel' aria-labelledby='consoles-tab'>".shipConsoleAdd($shipID)."</div>";
-        $form .= "<div class='tab-pane fade' id='finaldetails' role='tabpanel' aria-labelledby='finaldetails-tab'>".shipConsoleAdd($shipID)."</div>";
-        //$form .= "<div class='tab-pane fade' id='finaldetails' role='tabpanel' aria-labelledby='finaldetails-tab'>".finalBasicDetails($shipID)."</div>";
+        $form .= "<div class='tab-pane fade' id='finaldetails' role='tabpanel' aria-labelledby='finaldetails-tab'>".finalBasicDetails($shipID)."</div>";
         $form .= "</div>";
         return $form;
     }

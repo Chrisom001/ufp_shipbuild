8eb2af1a027ea79d3b1ba37d9e4efce4cad51dce
Chrisom001 <cwilkinson1988@gmail.com>
2025-03-08 15:46:06 +0000

    Updated menu Added image on ship build card on homepage Added ability to pull bridge officer slots

--- addShipBuild.php
+++ addShipBuild.php
@@ -18,6 +18,9 @@ function databaseError(){
     <!--- Stylesheet include -->
     <link href="css/style.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
 <?php echo $navbar; ?>

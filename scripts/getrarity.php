f24d7d500ef94022713b471cb8b6e66d62ed6700
Chrisom001 <cwilkinson1988@gmail.com>
2025-03-08 13:15:02 +0000

    Updated menu Added image on ship build card on homepage Added ability to pull bridge officer slots

--- scripts/getrarity.php
+++ scripts/getrarity.php
@@ -30,19 +30,21 @@
 //    echo "Failure!!!";/
 //}
 
-if (isset($_POST['value'])) {
-    $value = $_POST['value'];
-    if($value == 0){
-        echo "<option value=\"\">Please select a tier</option>";
-    } else {
-        $ships = getShipListByTier($value);
-        if ($ships) {
-            foreach ($ships as $ship) {
-                echo "<option value=\"{$ship['id']}\">{$ship['shipName']}</option>";
-            }
-        } else {
-            echo "<option value='0'>No options available</option>";
-        }
-    }
-}
+//if (isset($_POST['value'])) {
+//    $value = $_POST['value'];
+//    if ($value == 0) {
+//        echo "<option value=\"\">Please select a tier</option>";
+//    } else {
+//        $ships = getShipListByTier($value);
+//        if ($ships) {
+//            foreach ($ships as $ship) {
+//                echo "<option value=\"{$ship['id']}\">{$ship['shipName']}</option>";
+//            }
+//        } else {
+//            echo "<option value='0'>No options available</option>";
+//        }
+//    }
+//}
+
+echo "Test";
 ?>
